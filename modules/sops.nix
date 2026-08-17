584381def2d75eb4d48cf052966fcba612f10eeb
Jacob Karr <jacob@ningen.gg>
2026-08-17 11:24:55 -0400

    Push

--- modules/sops.nix
+++ modules/sops.nix
@@ -4,10 +4,4 @@
   sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
-  sops.secrets."caddy_env" = {
-    owner = "caddy";
-    mode = "0400";
-    restartUnits = [ "caddy.service" ];
-  };
-
 }
